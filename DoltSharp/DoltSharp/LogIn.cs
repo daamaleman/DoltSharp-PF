b4bf095b7dc5e2f09bcfe9ed7a06d1899c7c2087
daamaleman <diedereicha@uamv.edu.ni>
2024-11-11 07:16:53 -0600

    Agrego diseño de formulario y modelos

---
+++ DoltSharp/DoltSharp/LogIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoltSharp
+{
+    public partial class LogIn : MetroFramework.Forms.MetroForm
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
