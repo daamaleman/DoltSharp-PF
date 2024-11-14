6f44ba96ee55fb00cc63e77c9e9825d0b5d49aa5
daamaleman <diedereicha@uamv.edu.ni>
2024-11-13 21:51:50 -0600

    Agrego codigo sin bug

--- DoltSharp/DoltSharp/Project.Designer.cs
+++ DoltSharp/DoltSharp/Project.Designer.cs
@@ -41,45 +41,26 @@
             this.TxtDescription = new MetroFramework.Controls.MetroTextBox();
             this.DtpStartdate = new MetroFramework.Controls.MetroDateTime();
             this.DtpDeadline = new MetroFramework.Controls.MetroDateTime();
-<<<<<<< HEAD
-            this.BtnReturn = new MetroFramework.Controls.MetroButton();
-            this.TxtResource = new MetroFramework.Controls.MetroTextBox();
-=======
             this.CmbResource = new MetroFramework.Controls.MetroComboBox();
             this.BtnReturn = new MetroFramework.Controls.MetroButton();
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel1.Location = new System.Drawing.Point(110, 210);
-            this.metroLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(140, 19);
-=======
             this.metroLabel1.Location = new System.Drawing.Point(179, 258);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(146, 20);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel1.TabIndex = 1;
             this.metroLabel1.Text = "Nombre del proyecto:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-<<<<<<< HEAD
-            this.pictureBox1.Location = new System.Drawing.Point(226, 60);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(135, 116);
-=======
             this.pictureBox1.Location = new System.Drawing.Point(301, 74);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(180, 143);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -87,60 +68,34 @@
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel2.Location = new System.Drawing.Point(151, 242);
-            this.metroLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(79, 19);
-=======
             this.metroLabel2.Location = new System.Drawing.Point(201, 298);
             this.metroLabel2.Name = "metroLabel2";
             this.metroLabel2.Size = new System.Drawing.Size(85, 20);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel2.TabIndex = 2;
             this.metroLabel2.Text = "Descripcion:";
             // 
             // metroLabel3
             // 
             this.metroLabel3.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel3.Location = new System.Drawing.Point(151, 313);
-            this.metroLabel3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.metroLabel3.Name = "metroLabel3";
-            this.metroLabel3.Size = new System.Drawing.Size(85, 19);
-=======
             this.metroLabel3.Location = new System.Drawing.Point(201, 385);
             this.metroLabel3.Name = "metroLabel3";
             this.metroLabel3.Size = new System.Drawing.Size(90, 20);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel3.TabIndex = 3;
             this.metroLabel3.Text = "Fecha Limite:";
             // 
             // metroLabel4
             // 
             this.metroLabel4.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel4.Location = new System.Drawing.Point(151, 273);
-            this.metroLabel4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.metroLabel4.Name = "metroLabel4";
-            this.metroLabel4.Size = new System.Drawing.Size(99, 19);
-=======
             this.metroLabel4.Location = new System.Drawing.Point(201, 336);
             this.metroLabel4.Name = "metroLabel4";
             this.metroLabel4.Size = new System.Drawing.Size(105, 20);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel4.TabIndex = 4;
             this.metroLabel4.Text = "Fecha de Inicio:";
             // 
             // metroLabel5
             // 
             this.metroLabel5.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel5.Location = new System.Drawing.Point(58, 385);
-            this.metroLabel5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-=======
             this.metroLabel5.Location = new System.Drawing.Point(77, 474);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel5.Name = "metroLabel5";
             this.metroLabel5.Size = new System.Drawing.Size(0, 0);
             this.metroLabel5.TabIndex = 5;
@@ -148,31 +103,17 @@
             // metroLabel6
             // 
             this.metroLabel6.AutoSize = true;
-<<<<<<< HEAD
-            this.metroLabel6.Location = new System.Drawing.Point(96, 354);
-            this.metroLabel6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.metroLabel6.Name = "metroLabel6";
-            this.metroLabel6.Size = new System.Drawing.Size(154, 19);
-=======
             this.metroLabel6.Location = new System.Drawing.Point(160, 448);
             this.metroLabel6.Name = "metroLabel6";
             this.metroLabel6.Size = new System.Drawing.Size(165, 20);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.metroLabel6.TabIndex = 6;
             this.metroLabel6.Text = "Asigaciones de Recursos:";
             // 
             // BtnSave
             // 
-<<<<<<< HEAD
-            this.BtnSave.Location = new System.Drawing.Point(196, 414);
-            this.BtnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.BtnSave.Name = "BtnSave";
-            this.BtnSave.Size = new System.Drawing.Size(82, 54);
-=======
             this.BtnSave.Location = new System.Drawing.Point(350, 509);
             this.BtnSave.Name = "BtnSave";
             this.BtnSave.Size = new System.Drawing.Size(109, 66);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.BtnSave.TabIndex = 7;
             this.BtnSave.Text = "Guardar";
             this.BtnSave.UseSelectable = true;
@@ -184,28 +125,16 @@
             // 
             // 
             this.TxtName.CustomButton.Image = null;
-<<<<<<< HEAD
-            this.TxtName.CustomButton.Location = new System.Drawing.Point(110, 1);
-            this.TxtName.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.TxtName.CustomButton.Name = "";
-            this.TxtName.CustomButton.Size = new System.Drawing.Size(13, 14);
-=======
             this.TxtName.CustomButton.Location = new System.Drawing.Point(197, 1);
             this.TxtName.CustomButton.Name = "";
             this.TxtName.CustomButton.Size = new System.Drawing.Size(21, 21);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.TxtName.CustomButton.TabIndex = 1;
             this.TxtName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.TxtName.CustomButton.UseSelectable = true;
             this.TxtName.CustomButton.Visible = false;
             this.TxtName.Lines = new string[0];
-<<<<<<< HEAD
-            this.TxtName.Location = new System.Drawing.Point(262, 210);
-            this.TxtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-=======
             this.TxtName.Location = new System.Drawing.Point(350, 258);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtName.MaxLength = 32767;
             this.TxtName.Name = "TxtName";
             this.TxtName.PasswordChar = '\0';
@@ -214,11 +143,7 @@
             this.TxtName.SelectionLength = 0;
             this.TxtName.SelectionStart = 0;
             this.TxtName.ShortcutsEnabled = true;
-<<<<<<< HEAD
-            this.TxtName.Size = new System.Drawing.Size(164, 19);
-=======
             this.TxtName.Size = new System.Drawing.Size(219, 23);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtName.TabIndex = 8;
             this.TxtName.UseSelectable = true;
             this.TxtName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -230,28 +155,16 @@
             // 
             // 
             this.TxtDescription.CustomButton.Image = null;
-<<<<<<< HEAD
-            this.TxtDescription.CustomButton.Location = new System.Drawing.Point(110, 1);
-            this.TxtDescription.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.TxtDescription.CustomButton.Name = "";
-            this.TxtDescription.CustomButton.Size = new System.Drawing.Size(13, 14);
-=======
             this.TxtDescription.CustomButton.Location = new System.Drawing.Point(197, 1);
             this.TxtDescription.CustomButton.Name = "";
             this.TxtDescription.CustomButton.Size = new System.Drawing.Size(21, 21);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtDescription.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.TxtDescription.CustomButton.TabIndex = 1;
             this.TxtDescription.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.TxtDescription.CustomButton.UseSelectable = true;
             this.TxtDescription.CustomButton.Visible = false;
             this.TxtDescription.Lines = new string[0];
-<<<<<<< HEAD
-            this.TxtDescription.Location = new System.Drawing.Point(262, 242);
-            this.TxtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-=======
             this.TxtDescription.Location = new System.Drawing.Point(350, 295);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtDescription.MaxLength = 32767;
             this.TxtDescription.Name = "TxtDescription";
             this.TxtDescription.PasswordChar = '\0';
@@ -260,11 +173,7 @@
             this.TxtDescription.SelectionLength = 0;
             this.TxtDescription.SelectionStart = 0;
             this.TxtDescription.ShortcutsEnabled = true;
-<<<<<<< HEAD
-            this.TxtDescription.Size = new System.Drawing.Size(164, 19);
-=======
             this.TxtDescription.Size = new System.Drawing.Size(219, 23);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.TxtDescription.TabIndex = 9;
             this.TxtDescription.UseSelectable = true;
             this.TxtDescription.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -272,37 +181,14 @@
             // 
             // DtpStartdate
             // 
-<<<<<<< HEAD
-            this.DtpStartdate.Location = new System.Drawing.Point(262, 273);
-            this.DtpStartdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.DtpStartdate.MinimumSize = new System.Drawing.Size(0, 29);
-            this.DtpStartdate.Name = "DtpStartdate";
-            this.DtpStartdate.Size = new System.Drawing.Size(165, 29);
-=======
             this.DtpStartdate.Location = new System.Drawing.Point(350, 336);
             this.DtpStartdate.MinimumSize = new System.Drawing.Size(0, 30);
             this.DtpStartdate.Name = "DtpStartdate";
             this.DtpStartdate.Size = new System.Drawing.Size(219, 30);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.DtpStartdate.TabIndex = 10;
             // 
             // DtpDeadline
             // 
-<<<<<<< HEAD
-            this.DtpDeadline.Location = new System.Drawing.Point(262, 313);
-            this.DtpDeadline.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.DtpDeadline.MinimumSize = new System.Drawing.Size(0, 29);
-            this.DtpDeadline.Name = "DtpDeadline";
-            this.DtpDeadline.Size = new System.Drawing.Size(165, 29);
-            this.DtpDeadline.TabIndex = 11;
-            // 
-            // BtnReturn
-            // 
-            this.BtnReturn.Location = new System.Drawing.Point(322, 414);
-            this.BtnReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.BtnReturn.Name = "BtnReturn";
-            this.BtnReturn.Size = new System.Drawing.Size(82, 54);
-=======
             this.DtpDeadline.Location = new System.Drawing.Point(350, 385);
             this.DtpDeadline.MinimumSize = new System.Drawing.Size(0, 30);
             this.DtpDeadline.Name = "DtpDeadline";
@@ -324,53 +210,11 @@
             this.BtnReturn.Location = new System.Drawing.Point(500, 509);
             this.BtnReturn.Name = "BtnReturn";
             this.BtnReturn.Size = new System.Drawing.Size(109, 66);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.BtnReturn.TabIndex = 13;
             this.BtnReturn.Text = "Regresar";
             this.BtnReturn.UseSelectable = true;
             this.BtnReturn.Click += new System.EventHandler(this.BtnReturn_Click);
             // 
-<<<<<<< HEAD
-            // TxtResource
-            // 
-            // 
-            // 
-            // 
-            this.TxtResource.CustomButton.Image = null;
-            this.TxtResource.CustomButton.Location = new System.Drawing.Point(110, 1);
-            this.TxtResource.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.TxtResource.CustomButton.Name = "";
-            this.TxtResource.CustomButton.Size = new System.Drawing.Size(13, 14);
-            this.TxtResource.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.TxtResource.CustomButton.TabIndex = 1;
-            this.TxtResource.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.TxtResource.CustomButton.UseSelectable = true;
-            this.TxtResource.CustomButton.Visible = false;
-            this.TxtResource.Lines = new string[0];
-            this.TxtResource.Location = new System.Drawing.Point(262, 354);
-            this.TxtResource.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.TxtResource.MaxLength = 32767;
-            this.TxtResource.Name = "TxtResource";
-            this.TxtResource.PasswordChar = '\0';
-            this.TxtResource.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.TxtResource.SelectedText = "";
-            this.TxtResource.SelectionLength = 0;
-            this.TxtResource.SelectionStart = 0;
-            this.TxtResource.ShortcutsEnabled = true;
-            this.TxtResource.Size = new System.Drawing.Size(164, 19);
-            this.TxtResource.TabIndex = 14;
-            this.TxtResource.UseSelectable = true;
-            this.TxtResource.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.TxtResource.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
-            // 
-            // Project
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 477);
-            this.Controls.Add(this.TxtResource);
-            this.Controls.Add(this.BtnReturn);
-=======
             // Project
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -378,7 +222,6 @@
             this.ClientSize = new System.Drawing.Size(800, 587);
             this.Controls.Add(this.BtnReturn);
             this.Controls.Add(this.CmbResource);
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.Controls.Add(this.DtpDeadline);
             this.Controls.Add(this.DtpStartdate);
             this.Controls.Add(this.TxtDescription);
@@ -391,13 +234,7 @@
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
             this.Controls.Add(this.pictureBox1);
-<<<<<<< HEAD
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Project";
-            this.Padding = new System.Windows.Forms.Padding(15, 49, 15, 16);
-=======
-            this.Name = "Project";
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
             this.Text = "Agregar Proyecto";
             this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -420,12 +257,7 @@
         private MetroFramework.Controls.MetroTextBox TxtDescription;
         private MetroFramework.Controls.MetroDateTime DtpStartdate;
         private MetroFramework.Controls.MetroDateTime DtpDeadline;
-<<<<<<< HEAD
-        private MetroFramework.Controls.MetroButton BtnReturn;
-        private MetroFramework.Controls.MetroTextBox TxtResource;
-=======
         private MetroFramework.Controls.MetroComboBox CmbResource;
         private MetroFramework.Controls.MetroButton BtnReturn;
->>>>>>> 4aa385153075d94759c73e1ffa88ad5866aa0460
     }
 }
