f01c4d52575c78b62d2cf6acfba7e332b424c89b
elias <eamarin@uamv.edu.ni>
2024-11-17 21:23:50 -0600

    Agrego los Dao al los forms

---
+++ DoltSharp/DoltSharp/Dao/TaskDao.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DoltSharp.Dao
+{
+    internal class TaskDao
+    {
+        private List<Task> tasks;
+
+        public TaskDao()
+        {
+            tasks = new List<Task>();
+        }
+
+        // Devuelve todas las tareas
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+
+        // Agrega una nueva tarea a la lista
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        // Busca una tarea por ID
+        public Task FindTaskById(int id)
+        {
+            return tasks.Find(task => task.ID == id);
+        }
+
+        // Busca tareas por ID de proyecto
+        public List<Task> FindTasksByProjectId(int projectId)
+        {
+            return tasks.FindAll(task => task.projectID == projectId);
+        }
+
+        // Elimina una tarea por ID
+        public void DeleteTaskById(int id)
+        {
+            var task = FindTaskById(id);
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
+        }
+
+        // Actualiza una tarea
+        public void UpdateTask(int id, Task updatedTask)
+        {
+            int index = tasks.FindIndex(task => task.ID == id);
+
+            if (index != -1)
+            {
+                tasks[index] = updatedTask;
+            }
+        }
+}
